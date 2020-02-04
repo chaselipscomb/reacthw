c197bf83aaad8d2f1bfe8e032d409609fb6d4b9e
Chase Lipscomb <chaselipscomb@Chases-MacBook-Pro.local>
2020-02-04 11:29:00 -0800

    Function app comment

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Wrapper from "./components/Wrapper";
 import Header from "./components/Header";
 
 import "./App.css";
-
+// App function
 function App() {
   return (
     <div className="App">
