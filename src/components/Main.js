9eab5d5097f8b288ad61f757b4c62a78c7aeac83
Chase Lipscomb <chaselipscomb@Chases-MacBook-Pro.local>
2020-02-04 11:35:49 -0800

    main component comment

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import DataArea from "./DataArea";
 import "../styles/Main.css";
-
+// main component
 export default class Main extends Component {
   render() {
     return (
